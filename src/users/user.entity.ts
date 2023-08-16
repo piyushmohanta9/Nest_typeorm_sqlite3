ebe1f483e00bf5866a11dea41f62454981a5e6f8
Piyush Mohanta <61897664+piyushmohanta9@users.noreply.github.com>
2023-08-16 11:21:19 +0530

    created entities

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,10 +1,15 @@
 import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
 
+
+@Entity()
 export class User{
 
+    @PrimaryGeneratedColumn()
     id: string;
 
+    @Column()
     email: string;
 
+    @Column()
     password: string;
 }
