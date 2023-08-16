9a9c7eb2516401a8c219cbead04166f193d89f4a
Piyush Mohanta <61897664+piyushmohanta9@users.noreply.github.com>
2023-08-16 11:39:58 +0530

    added entity to app

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,6 +5,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UsersService],
   controllers: [UsersController]
 })
