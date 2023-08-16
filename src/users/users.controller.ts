267e54792c49c7bd3cde75b624a124f361b6b9f2
Piyush Mohanta <61897664+piyushmohanta9@users.noreply.github.com>
2023-08-16 17:17:07 +0530

    connected user repository to user services

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get, Post, Param, Body } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('auth')
 export class UsersController {
 
+    constructor(private userService: UsersService){}
+
     @Post('signup')
-    createSecureServer(@Body() body:CreateUserDto){
-        console.log(body);
+    createUser(@Body() body:CreateUserDto){
+        this.userService.create(body.email, body.password);
     }
     
 
