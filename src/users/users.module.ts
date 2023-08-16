2d96b03f4811eaad4ac96c04036d34f8c63938df
Piyush Mohanta <61897664+piyushmohanta9@users.noreply.github.com>
2023-08-16 11:37:35 +0530

    added user entity to user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Module({
   providers: [UsersService],
